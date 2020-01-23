b92a12e22c4b6b6f289e135066dbe73433c208bf
Paul Smith <p@ulsmith.net>
2020-01-23 13:33:43 +0000

    some changes due to model mehtod names and verify jwt working

--- src/Service/Auth.js
+++ src/Service/Auth.js
@@ -40,28 +40,29 @@ class Auth extends Service {
 		let userModel = new UserModel();
 		let userAccountModel = new UserAccountModel();
 
-		return userModel.getFromCredentials(username).then((user) => {
-			// NOTE: need to flood prevent here too
-			if (!user) throw new RestError('Login details incorrect, please try again...', 401);
-            if (user.password !== Crypto.passwordHash(password, user.password.substring(0, user.password.length / 2))) throw new RestError('Login details incorrect, please try again...', 401);
-			
-			return user;
-		}).then((user) => {
-			let date = new Date();
+		return userModel.getAuthedFromEmail(username)
+			.then((user) => {
+				// NOTE: need to flood prevent here too
+				if (!user) throw new RestError('Login details incorrect, please try again.', 401);
+				if (user.password !== Crypto.passwordHash(password, user.password.substring(0, user.password.length / 2))) throw new RestError('Login details incorrect, please try again.', 401);
+				
+				return user;
+			}).then((user) => {
+				let date = new Date();
 
-			// update user but return data from before update so last logged on correct
-			return userAccountModel
-				.update(user.id, { login_current: date , login_previous: user.login_current })
-				.then((update) => ({
-					uuid: user.uuid, 
-					email: user.email, 
-					login_current: date, 
-					login_previous: user.login_current
-				}));
-		}).then((user) => {
-			// construct output and inject header
-			return { authorization: this.generateJWT(user), user: user };
-		});
+				// update user but return data from before update so last logged on correct
+				return userAccountModel
+					.update(user.id, { login_current: date , login_previous: user.login_current })
+					.then((update) => ({
+						uuid: user.uuid, 
+						email: user.email, 
+						login_current: date, 
+						login_previous: user.login_current
+					}));
+			}).then((user) => {
+				// construct output and inject header
+				return { token: this.generateJWT(user), user: user };
+			});
 	}
 
     /**
@@ -73,41 +74,40 @@ class Auth extends Service {
 	verify(authorization) {
 		let payload;
 		let jwt = authorization.replace('Bearer', '').trim();
-		if (!jwt) throw new RestError('Login details incorrect, please try again...', 401);
 
 		try {
 			payload = this.verifyJWT(jwt);
 		} catch(error) {
-			throw new RestError('Invalid JWT', 401);
+			if (error.name === 'TokenExpiredError') {
+				throw new RestError({ 
+					message: 'Authorization expired, please refresh expired token.', 
+					method: 'POST', 
+					url: this.$environment.JWTIssuer + '/account/refresh',
+					body: {token: jwt}
+				}, 401);
+			}
+			
+			throw new RestError({
+				message: 'Authorization failed, please log in to authorize.',
+				method: 'POST',
+				url: this.$environment.JWTIssuer + '/account/authenticate',
+				body: { username: '', password: '' }
+			}, 401);
 		}
-
-// { iss: 'https://cvapi.razilo.net',
-//   aud: 'https://cv.razilo.net',
-//   iat: 1561063121,
-//   nbf: 1561063121,
-//   exp: 1561063721,
-//   guid: 11 }
 
 		let userModel = new UserModel();
 
-		return userModel.getFromUUID(payload.guid).then((data) => {
-			// NOTE: need to flood prevent here too
-			if (!data || !data.id) throw new RestError('User not found, please try again...', 401);
+		return userModel.getAuthedFromUUID(payload.uuid)
+			.then((user) => {
+				if (!user) throw new RestError('User not found, please try again.', 404);
 
-			console.log(data);
-			// return {name: data.name, email: data.email, last_logged_in: };
-			return {};
-		})
-
-		console.log('[DONE] Check valid jwt data');
-		console.log('get the user');
-		console.log('verify a pin or some other data is correct to lock it down');
-		console.log('save the user to this.user');
-		console.log('return, so this can be used to ping and also to set th euser in the service for hwen we do more');
-		
-
-
-		return Promise.resolve({ 'key': 'ping' });
+				return { user: {
+					uuid: user.uuid,
+					email: user.email,
+					login_current: user.login_current,
+					login_previous: user.login_previodus
+				}};
+			});
 	}
 
     /**
@@ -118,7 +118,7 @@ class Auth extends Service {
      */
 	generateJWT(user) {
 		return JWT.sign({
-			iss: 'http://127.0.0.1:3000',
+			iss: this.$environment.JWTIssuer,
 			aud: this.$client.origin,
 			iat: Math.floor(Date.now() / 1000),
 			nbf: Math.floor(Date.now() / 1000),
