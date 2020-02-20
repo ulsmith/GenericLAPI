4de5c2f01ecfa4a71ad574bb7fbcd584b5a90e17
Paul Smith <p@ulsmith.net>
2020-02-20 19:42:22 +0000

    Added first view, for emails, using newfuncs

--- src/View/Email/PasswordReset.js
+++ src/View/Email/PasswordReset.js
@@ -1,37 +1,85 @@
 'use strict';
 
-const html = (...props) => props[0].reduce((acc, cur, idx) => acc + props[idx] + cur);
+const DataTools = require('../../Library/DataTools.js');
 
-const PasswordReset = (props) => html`
-	<h1>Boom</h1>
-	<p>Hello ${props.name} how are <strong>YOU</strong> doing you ${props.age}.</p>
+/**
+ * @namespace API/View/Email
+ * @class PasswordResetHtml
+ * @description Password reset email to send out to users, splice in properties as template literals
+ * @author Paul Smith (ulsmith) <p@ulsmith.net> <pa.ulsmith.net>
+ * @copyright 2020 Paul Smith (ulsmith) all rights reserved
+ * @license MIT 
+ */
+const PasswordResetHtml = (props) => DataTools.html`
+	<style>
+		* { font-family: arial, sans-serif; }
+		body { background-color: #ccc; }
+		.header { padding: 20px; background-color: #444; }
+		.main { padding: 20px; background-color: #eee; }
+		.footer { padding: 20px; }
+		.title { color: #ddd; }
+		p { font-size: 18px; color: #555; }
+		.text-center { text-align: center; }
+		.text-link { color: grey; font-style: italic; font-weight: bold; }
+		.text-button { padding: 20px; display: inline-block; background-color: #4848c7; margin: 10px; color: white; text-decoration: none; font-size: 20px; }
+		.signature { font-size: 20px; color: #222; }
+	</style>
+
+	<div class="header">
+		<h1 class="title">Hi ${props.name}!</h1>
+	</div>
+	<div class="main">
+		<p>We have had a password reset request come through for your account on <strong><a href="${props.systemUrl}">${props.systemName}</a></strong></p>
+		<p>If this was not you that requested this, you may ignore this message; password requests expire after ten minutes so don't panic, your all secure.</p>
+		<p>If you did make this request, great; simply use the button below to to access our system and change your password.</p>
+		<p>Its always a good idea to check the domain password reset point you to, make sure it matches the website when others send you requests!</p>
+		<p class="text-center"><a href="${props.token}" class="text-button">Reset your Passsword</a></p>
+		<p class="text-center text-link">${props.token}</p>
+		<p>Thanks again for using ${props.systemName}!</p>
+		<p class="signature">The ${props.systemName} Team.</p>
+	</div>
+	<div class="footer">
+		<p>${props.systemUrl}</p>
+		<p>${props.systemName}</p>
+	</div>
 `;
 
-module.exports = PasswordReset;
-
-// const Email = require('./Email.js');
-
-// /**
-//  * @namespace API/View/Email
-//  * @class Controller
-//  * @extends Email
-//  * @description System class to give a base for creating controllers, exposing services and base methods
-//  * @author Paul Smith (ulsmith) <p@ulsmith.net> <pa.ulsmith.net>
-//  * @copyright 2020 Paul Smith (ulsmith) all rights reserved
-//  * @license MIT 
-//  */
-// class Controller extends Email {
-
-// 	/**
-// 	 * @public @method constructor
-// 	 * @description Base method when instantiating class
-// 	 */
-// 	constructor() {
-// 		super();
-// 	}
+/**
+ * @namespace API/View/Email
+ * @class PasswordResetText
+ * @description Password reset email to send out to users, splice in properties as template literals
+ * @author Paul Smith (ulsmith) <p@ulsmith.net> <pa.ulsmith.net>
+ * @copyright 2020 Paul Smith (ulsmith) all rights reserved
+ * @license MIT 
+ */
+const PasswordResetText = (props) => DataTools.text`
+	Hi ${props.name}!
 
 
-// }
+	We have had a password reset request come through for your account on <strong><a href="${props.systemUrl}">${props.systemName}</a></strong></p>
+	
+	If this was not you that requested this, you may ignore this message; password requests expire after ten minutes so don't panic, your all secure.</p>
+	
+	If you did make this request, great; simply use the button below to to access our system and change your password.</p>
+	
+	Its always a good idea to check the domain password reset point you to, make sure it matches the website when others send you requests!</p>
+	
+	-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	
+	${props.token}
+	
+	-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+	
+	Thanks again for using ${props.systemName}!</p>
+	
+	The ${props.systemName} Team.</p>
+	
+	.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.
+	
+	${props.systemUrl}</p>
+	${props.systemName}</p>
+	
+	'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'
+`;
 
-// module.exports = Controller;
-
+module.exports = { PasswordResetHtml, PasswordResetText };
