4396a14345c98e992f6806962bdd8748c3bf70ff
Paul Smith <p@ulsmith.net>
2020-08-17 10:09:17 +0100

    missing email model

---
+++ src/Model/DatabaseName/Public/EmailBlocked.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Model = require('../../../System/Model.js');
+
+/**
+ * @namespace API/Model/Dbduck/Public
+ * @class EmailBlocked
+ * @extends Model
+ * @description Model class for public.email_blocked table
+ * @author Paul Smith (ulsmith) <p@ulsmith.net> <pa.ulsmith.net>
+ * @copyright 2020 Paul Smith (ulsmith) all rights reserved
+ * @license MIT
+ */
+class EmailBlocked extends Model {
+
+	/**
+	 * @public @method constructor
+	 * @description Base method when instantiating class
+	 */
+    constructor() {
+		super('dbduck', 'public.email_blocked');
+	}
+}
+
+module.exports = EmailBlocked;
